28e497944592c8816d1da0ecb6ee1eb8abdc9e25
“Alex <2gdaw03@ARRIAGAINFO.LOCAL>
2023-10-02 09:29:23 +0200

    Creado archivo para leer variables y las funciones para leer las variables del plc en js

--- index.js
+++ index.js
@@ -16,3 +16,31 @@ function empezarAnimacion() {
   btnMartxa.classList.add("active")
   imgTranvia.classList.add("homePrimera")
 }
+
+//Variables del plc que se utilizaran en la web
+let homing;
+let destino;
+let confirmar_destino;
+let inicio;
+let orden_mover;
+let prueba = new Array();
+
+setInterval(() =>fetch("sites/leer_variables.html")
+        .then(response => response.text())
+        .then(datos => prueba = datos.split("/")), 500);
+
+setInterval(() =>{
+  homing = prueba[0];
+  destino = prueba[1];
+  confirmar_destino = prueba[2];
+  inicio = prueba[3];
+  orden_mover = prueba[4];
+}, 500);
+
+setInterval(() => {
+  document.getElementById("homing").innerHTML = "Homing: " + homing;
+  document.getElementById("destino").innerHTML = "destino: " + destino;
+  document.getElementById("c_destino").innerHTML = "confirmar_destino: " + confirmar_destino;
+  document.getElementById("inicio").innerHTML = "inicio: " + inicio;
+  document.getElementById("o_mover").innerHTML = "orden_mover: " + orden_mover;
+}, 500);
