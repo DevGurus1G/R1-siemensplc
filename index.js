b4a3164a0c624656297fddad629d82235e0e5016
“Alex <2gdaw03@arriagainfo.local>
2023-10-02 08:31:51 +0200

    Borrar lineas de codigo innecesarias

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-let animaciones = []
 // constantes de los botones e img tranvia
 const btnMartxa = document.querySelector("#martxa")
 const btnAbrirPuertas = document.querySelector("#abrirPuertas")
-const btnAvance = document.querySelector("#avance")
-const btnRetroceso = document.querySelector("#retroceso")
 
 const imgTranvia = document.querySelector(".tranvia")
-
-btnMartxa.addEventListener("click", empezarAnimacion)
-btnAvance.addEventListener("mousedown", (e) => moverseAvanzar(e))
-btnAvance.addEventListener("mouseup", (e) => moverseAvanzar(e))
-btnRetroceso.addEventListener("mousedown", (e) => moverseRetroceder(e))
-btnRetroceso.addEventListener("mouseup", (e) => moverseRetroceder(e))
 
 let animacionesNombre = [
   "homePrimera",
@@ -20,61 +11,8 @@ let animacionesNombre = [
   "terceraCuarta",
   "cuartaQuinta",
 ]
-let parar = false
-let leftPosiciones = ["16%", "32%", "48%", "64%", "80%"]
+
 function empezarAnimacion() {
   btnMartxa.classList.add("active")
   imgTranvia.classList.add("homePrimera")
-}
-
-let animacion = null
-let retrocediendo = false
-let tiempoTranscurrido = 0
-
-function moverseAvanzar(e) {
-  if (e.type == "mousedown") {
-    btnAvance.classList.add("active")
-
-    if (!retrocediendo) {
-      // Iniciar la animación solo si no estamos retrocediendo
-      animacion = imgTranvia.animate([{ left: "16%" }, { left: "80%" }], {
-        duration: 5000,
-        iterations: Infinity,
-        fill: "forwards",
-        playbackRate: 1,
-        delay: -tiempoTranscurrido,
-      })
-    }
-  } else {
-    if (animacion) {
-      btnAvance.classList.remove("active")
-      tiempoTranscurrido = animacion.currentTime
-      animacion.pause()
-    }
-  }
-}
-
-function moverseRetroceder(e) {
-  if (e.type == "mousedown") {
-    btnRetroceso.classList.add("active")
-
-    if (retrocediendo) {
-      // Iniciar la animación solo si estamos retrocediendo
-      animacion = imgTranvia.animate([{ left: "80%" }, { left: "16%" }], {
-        duration: 5000,
-        iterations: Infinity,
-        fill: "forwards",
-        playbackRate: -1,
-        delay: -tiempoTranscurrido,
-      })
-    }
-  } else {
-    if (animacion) {
-      btnRetroceso.classList.remove("active")
-      tiempoTranscurrido = animacion.currentTime
-      animacion.pause()
-    }
-  }
-
-  retrocediendo = !retrocediendo // Cambiar la dirección de retroceso
 }
