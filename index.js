92f2e3851da1ba89a403b9e5769da6f67c0454d6
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2023-10-04 13:50:40 +0200

    Animacion para probar

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const desplegableDestino = document.querySelector("#paradas")
 const imgTranvia = document.querySelector(".tranvia")
 const pilotoEncendido = document.querySelector(".estado")
 const leyendaPiloto = pilotoEncendido.querySelector(".leyenda")
-const bdd = '"DATOSWEB"';
+const bdd = '"DATOSWEB"'
 
 let animacionesNombre = [
   "primeraSegunda",
@@ -18,6 +18,7 @@ let animacionesNombre = [
   "terceraSegunda",
   "segundaPrimera",
 ]
+const arrayCoincidentePosicion = [1, 2, 3, 4, 3, 2, 1, 0]
 
 function empezarAnimacion() {
   btnInicio.classList.add("active")
@@ -32,6 +33,7 @@ let inicio
 let orden_mover
 // Array donde se guardan todas las variables que se leen del archivo "leer_variables.html"
 let arrayVariablesPlc = new Array()
+// Variable para saber en que parada esta actualmente el tranvia
 
 setInterval(
   () =>
@@ -49,65 +51,123 @@ setInterval(() => {
   orden_mover = arrayVariablesPlc[4]
 }, 500)
 
-setInterval(() => movimientoCiclico(), 1000)
+setInterval(() => movimientoCiclico(), 500)
+
+// setInterval(() => {
+//   console.log("destino "+destino)
+//   console.log("cdest " + confirmar_destino)
+//   console.log("inicio "+inicio)
+//   console.log("o_mover "+orden_mover)
+//   console.log("homing" + homing)
+// }, 500)
+
+/*
+do
+{
+
+  
+
+  homingParada0()
+  
+}
+while(homing != "1")*/
 
 //Funciones para la logica del movimiento
 
 function homingParada0() {
   if (homing == "1") {
-    pilotoEncendido.style.backgroundColor = "#006f2b";
-    leyendaPiloto.textContent = "Encendido";
+    //TODO Aqui va el codigo para la animacion de cochera a parada 0
+    pilotoEncendido.style.backgroundColor = "#006f2b"
+    leyendaPiloto.textContent = "Encendido"
   }
 }
 
-let posicionActual = 0
+let contador = 0
 let entrado = false
 function movimientoCiclico() {
   if (inicio == "1" || inicio == 1) {
-	  if(!entrado) {
-		 imgTranvia.classList.add("homePrimera")
-		 entrado = true
-	  }
+    if (!entrado) {
+      imgTranvia.classList.add("homePrimera")
+      entrado = true
+    }
     //TODO Aqui va el codigo para que empieze la animacion ciclica
     if (comprobarListoMoverCiclico()) {
-      if (posicionActual == 8) posicionActual = 0
-      empezarAnimacion(posicionActual)
-      posicionActual++
+      if (contador == 8) contador = 0
+      empezarAnimacion(contador)
+      contador++
     }
   }
 }
 
+function movimientoManual(destino) {
+  const posiciones = encontrarPosicionesDestino(destino)
+
+  if (destino < contador) {
+    for (
+      let i = contador;
+      i >= 0 && destino != arrayCoincidentePosicion[posiciones[0]];
+      i--
+    ) {
+      empezarAnimacion(contador)
+      contador++
+    }
+  } else {
+    for (
+      let i = contador;
+      i <= animacionesNombre.length &&
+      destino != arrayCoincidentePosicion[posiciones[1]];
+      i++
+    ) {
+      empezarAnimacion(contador)
+      contador--
+    }
+  }
+}
+
+function encontrarPosicionesDestino(destino) {
+  const posiciones = []
+
+  for (let i = 0; i < arrayCoincidentePosicion.length; i++) {
+    if (arrayCoincidentePosicion[i] === destino) {
+      posiciones.push(i)
+    }
+  }
+
+  return posiciones
+}
+
 function comprobarListoMoverCiclico() {
-  // Funcion que devuelve un true cuando esta listo para moverse
+  // Funcion que devuelve un 1 cuando esta listo para moverse
   return orden_mover == 1 ? true : false
 }
 
-function empezarAnimacion(posicionActual) {
+function empezarAnimacion(contador) {
   btnInicio.classList.add("active")
   imgTranvia.classList.remove(...animacionesNombre)
-  imgTranvia.classList.add(animacionesNombre[posicionActual])
+  imgTranvia.classList.add(animacionesNombre[contador])
 }
 
-function mandarDatos(variable,valor){
+function mandarDatos(variable, valor) {
   fetch(window.location.href, {
-    method: 'POST',
-    body: '' + bdd + '.' + variable + '=' + valor + '', 
+    method: "POST",
+    body: "" + bdd + "." + variable + "=" + valor + "",
     headers: {
-        'Content-Type': 'text/plain'
-    }
-})
+      "Content-Type": "text/plain",
+    },
+  })
 }
 
-//Eventos cuando se clicka botones para mandar datos
+//Eventos cuando se clicka para mandar datos
 
 btnInicio.addEventListener("click", () => {
-	mandarDatos("INICIO",1)
-});
-btnParar.addEventListener("click", () => mandarDatos("INICIO",0));
+  mandarDatos("INICIO", 1)
+})
+btnParar.addEventListener("click", () => mandarDatos("INICIO", 0))
 btnConfirmarDestino.addEventListener("click", () => {
-	mandarDatos("CONFIRMAR_DESTINO",1);
-	setTimeout(() => mandarDatos("CONFIRMAR_DESTINO", 0), 500)
+  mandarDatos("CONFIRMAR_DESTINO", 1)
+  movimientoManual(desplegableDestino.value)
+  setTimeout(() => mandarDatos("CONFIRMAR_DESTINO", 0), 500)
 })
 desplegableDestino.addEventListener("click", () => {
-	mandarDatos("DESTINO", desplegableDestino.value)
-});
+  mandarDatos("DESTINO", desplegableDestino.value)
+})
