40527f40db9757366080cef74b51fcc1f856d6a5
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2023-10-05 13:53:24 +0200

    Quitado funciones y arreglado el localStorage

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // constantes de los botones e img tranvia
-const btnInicio = document.querySelector("#inicio");
-btnInicio.disabled = true;
-const btnParar = document.querySelector("#parar");
-btnParar.disabled = true;
-const btnConfirmarDestino = document.querySelector("#confirmarDestino");
-btnConfirmarDestino.disabled = true;
-const desplegableDestino = document.querySelector("#paradas");
-desplegableDestino.disabled = true;
+const btnInicio = document.querySelector("#inicio")
+btnInicio.disabled = true
+const btnParar = document.querySelector("#parar")
+btnParar.disabled = true
+const btnConfirmarDestino = document.querySelector("#confirmarDestino")
+btnConfirmarDestino.disabled = true
+const desplegableDestino = document.querySelector("#paradas")
+desplegableDestino.disabled = true
 const imgTranvia = document.querySelector(".tranvia")
 const pilotoEncendido = document.querySelector(".estado")
 const leyendaPiloto = pilotoEncendido.querySelector(".leyenda")
-const bdd = '"DATOSWEB"';
+const bdd = '"DATOSWEB"'
 
 let animacionesNombre = [
   "primeraSegunda",
@@ -58,35 +58,30 @@ setInterval(() => movimientoCiclico(), 1000)
 //Funcionas y Promesa para comenzar el programa con el homing
 
 setInterval(() => {
-  if(homing == 1){
+  if (homing == 1) {
     homingEncendido()
-  }else{
+  } else {
     homingApagado()
   }
-}, 1000);
+}, 1000)
 
 function homingEncendido() {
-  
   btnInicio.disabled = false
   btnParar.disabled = false
   btnConfirmarDestino.disabled = false
   desplegableDestino.disabled = false
-  pilotoEncendido.style.backgroundColor = "#006f2b";
-  leyendaPiloto.textContent = "Encendido";
-
+  pilotoEncendido.style.backgroundColor = "#006f2b"
+  leyendaPiloto.textContent = "Encendido"
 }
 
 function homingApagado() {
-  
   btnInicio.disabled = true
   btnParar.disabled = true
   btnConfirmarDestino.disabled = true
   desplegableDestino.disabled = true
-  pilotoEncendido.style.backgroundColor = "#dc2424";
-  leyendaPiloto.textContent = "Apagado";
-
+  pilotoEncendido.style.backgroundColor = "#dc2424"
+  leyendaPiloto.textContent = "Apagado"
 }
-
 
 //Funciones para la logica del movimiento
 
@@ -97,20 +92,19 @@ function movimientoCiclico() {
   if (inicio == 1) {
     //TODO Aqui va el codigo para que empieze la animacion ciclica
     if (comprobarListoMoverCiclico()) {
-      if(!entrado) {
+      if (!entrado) {
         imgTranvia.classList.add("homePrimera")
         entrado = true
         ejecutado = true
-       }
-      if(!ejecutado)
-      {
+      }
+      if (!ejecutado) {
         if (posicionActual == 8) posicionActual = 0
         empezarAnimacion(posicionActual)
         posicionActual++
         ejecutado = true
       }
-    }else{
-        ejecutado = false
+    } else {
+      ejecutado = false
     }
   }
 }
@@ -126,14 +120,14 @@ function empezarAnimacion(posicionActual) {
   imgTranvia.classList.add(animacionesNombre[posicionActual])
 }
 
-function mandarDatos(variable,valor){
+function mandarDatos(variable, valor) {
   fetch(window.location.href, {
-    method: 'POST',
-    body: '' + bdd + '.' + variable + '=' + valor + '', 
+    method: "POST",
+    body: "" + bdd + "." + variable + "=" + valor + "",
     headers: {
-        'Content-Type': 'text/plain'
-    }
-})
+      "Content-Type": "text/plain",
+    },
+  })
 }
 
 //Movimiento Manua
@@ -166,74 +160,72 @@ function movimientoManual(destinoSeleccionado) {
 //Eventos cuando se clicka botones para mandar datos
 
 btnInicio.addEventListener("click", () => {
-	mandarDatos("INICIO",1)
-});
-btnParar.addEventListener("click", () => mandarDatos("INICIO",0));
+  mandarDatos("INICIO", 1)
+})
+btnParar.addEventListener("click", () => mandarDatos("INICIO", 0))
 btnConfirmarDestino.addEventListener("click", () => {
-	mandarDatos("CONFIRMAR_DESTINO",1);
-  elegirDireccion()
-  reproducirSecuencialmente(paradasSeleccionadas)
-	setTimeout(() => mandarDatos("CONFIRMAR_DESTINO", 0), 500);
-  guardarEstadisticas();
+  mandarDatos("CONFIRMAR_DESTINO", 1)
+  guardarEstadisticas()
+  setTimeout(() => mandarDatos("CONFIRMAR_DESTINO", 0), 500)
 })
 desplegableDestino.addEventListener("click", () => {
-	mandarDatos("DESTINO", desplegableDestino.value)
-});
+  mandarDatos("DESTINO", desplegableDestino.value)
+})
 
 //Funciones para las estadisticas
-let stats;
-function guardarEstadisticas(){
-	if(localStorage.getItem("estadistica")==null){
-		stats = {
-			parada0: 0,
-			parada1: 0,
-			parada2: 0,
-			parada3: 0,
-			parada4: 0,
-		};
-		localStorage.setItem("estadistica", JSON.stringify(stats));
-	}else{
-		stats = JSON.parse(localStorage.getItem("estadistica"));
-		switch(destino){
-			case 0:
-				stats.parada0 += 1;
-				break;
-			case 1:
-				stats.parada1 += 1;
-				break;
-			case 2:
-				stats.parada2 += 1;
-				break;
-			case 3:
-				stats.parada3 += 1;
-				break;
-			case 4:
-				stats.parada4 += 1;
-				break;
-		}
-		localStorage.setItem("estadistica", JSON.stringify(stats))
-	}
+let stats
+function guardarEstadisticas() {
+  if (localStorage.getItem("estadistica") == null) {
+    stats = {
+      parada0: 0,
+      parada1: 0,
+      parada2: 0,
+      parada3: 0,
+      parada4: 0,
+    }
+    localStorage.setItem("estadistica", JSON.stringify(stats))
+  } else {
+    stats = JSON.parse(localStorage.getItem("estadistica"))
+    switch (destino) {
+      case 0:
+        stats.parada0 += 1
+        break
+      case 1:
+        stats.parada1 += 1
+        break
+      case 2:
+        stats.parada2 += 1
+        break
+      case 3:
+        stats.parada3 += 1
+        break
+      case 4:
+        stats.parada4 += 1
+        break
+    }
+    localStorage.setItem("estadistica", JSON.stringify(stats))
+  }
 }
 
 //Funciones para posicion actual
 
 window.addEventListener("unload", (event) => {
-  localStorage.setItem("posicion", posicionActual);
-});
-  
+  localStorage.setItem("posicion", posicionActual)
+})
+
 window.addEventListener("load", (event) => {
-posicionActual = localStorage.getItem("posicion");
-});
+  posicionActual = localStorage.getItem("posicion")
+})
 
 //Funciones para cambiar orden mover desde la web (Para realizar las pruebas)
 document.getElementById("enOrdenMover").addEventListener("click", () => {
-mandarDatos("ORDEN_MOVER",1);
-});
+  mandarDatos("ORDEN_MOVER", 1)
+})
 
 document.getElementById("apOrdenMover").addEventListener("click", () => {
-mandarDatos("ORDEN_MOVER",0);
-});
-  
-setInterval(()=>{
-document.getElementById("tOrdenMover").innerHTML = orden_mover;
-},100)
+  mandarDatos("ORDEN_MOVER", 0)
+})
+
+setInterval(() => {
+  document.getElementById("tOrdenMover").innerHTML = orden_mover
+}, 100)
