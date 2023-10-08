41e0997ba9c3f11369401233a01730956b1f5277
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2023-10-08 16:58:30 +0200

    Añadidos los comentarios para JSDoc

--- estadisticas.js
+++ estadisticas.js
@@ -1,11 +1,36 @@
+/**
+ * Obtiene los datos de estadísticas almacenados en el almacenamiento local (localStorage) y los parsea como un objeto JSON.
+ * @type {Object}
+ */
 const dataStorage = JSON.parse(localStorage.getItem("estadistica"));
 
+/**
+ * Elemento del DOM que representa el gráfico de barras.
+ * @type {HTMLCanvasElement}
+ */
 const graph = document.querySelector("#grafico");
+
+/**
+ * Configuración de datos para el gráfico de barras.
+ * @type {Object}
+ */
 const data = {
   datasets: [
     {
+      /**
+       * Etiqueta del conjunto de datos del gráfico.
+       * @type {string}
+       */
       label: "Paradas",
+      /**
+       * Datos de estadísticas de paradas obtenidos del almacenamiento local.
+       * @type {number[]}
+       */
       data: dataStorage,
+      /**
+       * Color de fondo de las barras del gráfico.
+       * @type {string[]}
+       */
       backgroundColor: [
         "rgba(1, 38, 34, 0.2)",
         "rgba(49, 38, 34, 0.2)",
@@ -17,25 +42,89 @@ const data = {
   ],
 };
 
+/**
+ * Configuración general del gráfico de barras.
+ * @type {Object}
+ */
 const config = {
+  /**
+   * Tipo de gráfico: gráfico de barras.
+   * @type {string}
+   */
   type: "bar",
+  /**
+   * Datos utilizados para el gráfico.
+   * @type {Object}
+   */
   data: data,
+  /**
+   * Opciones de configuración para el gráfico.
+   * @type {Object}
+   */
   options: {
+    /**
+     * Habilita la capacidad de respuesta del gráfico para adaptarse al tamaño de la pantalla.
+     * @type {boolean}
+     */
     responsive: true,
+    /**
+     * Plugins del gráfico, incluyendo el título.
+     * @type {Object}
+     */
     plugins: {
+      /**
+       * Configuración del título del gráfico.
+       * @type {Object}
+       */
       title: {
+        /**
+         * Indica si se debe mostrar el título en el gráfico.
+         * @type {boolean}
+         */
         display: true,
-        text: "Las paradas que mas se toman",
+        /**
+         * Texto del título del gráfico.
+         * @type {string}
+         */
+        text: "Las paradas que más se toman",
       },
     },
+    /**
+     * Configuración de las escalas en el gráfico.
+     * @type {Object}
+     */
     scales: {
+      /**
+       * Configuración de la escala X (horizontal).
+       * @type {Object}
+       */
       x: {
+        /**
+         * Configuración de la cuadrícula en la escala X.
+         * @type {Object}
+         */
         grid: {
+          /**
+           * Indica si se debe mostrar la cuadrícula en la escala X.
+           * @type {boolean}
+           */
           display: false,
         },
       },
+      /**
+       * Configuración de la escala Y (vertical).
+       * @type {Object}
+       */
       y: {
+        /**
+         * Configuración de la cuadrícula en la escala Y.
+         * @type {Object}
+         */
         grid: {
+          /**
+           * Indica si se debe mostrar la cuadrícula en la escala Y.
+           * @type {boolean}
+           */
           display: false,
         },
       },
@@ -43,4 +132,7 @@ const config = {
   },
 };
 
+/**
+ * Crea un nuevo gráfico de barras en el elemento especificado del DOM utilizando la configuración proporcionada.
+ */
 new Chart(graph, config);
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// Constantes
+/**
+ * Constantes utilizadas en la aplicación.
+ * @constant
+ * @type {HTMLElement}
+ */
 const btnInicio = document.querySelector("#inicio");
 const btnParar = document.querySelector("#parar");
 const btnConfirmarDestino = document.querySelector("#confirmarDestino");
@@ -6,8 +10,18 @@ const desplegableDestino = document.querySelector("#paradas");
 const imgTranvia = document.querySelector(".tranvia");
 const pilotoEncendido = document.querySelector(".estado");
 const leyendaPiloto = pilotoEncendido.querySelector(".leyenda");
+
+/**
+ * Base de datos utilizada para mandar datos al PLC.
+ * @constant
+ * @type {string}
+ */
 const bdd = '"DATOSWEB"';
 
+/**
+ * Nombres de las animaciones utilizadas.
+ * @type {string[]}
+ */
 let animacionesNombre = [
   "primeraSegunda",
   "segundaTercera",
@@ -19,18 +33,32 @@ let animacionesNombre = [
   "segundaPrimera",
 ];
 
-//Variables del plc que se utilizaran en la web
+/**
+ * Variables del PLC que se utilizan en la web.
+ * @type {number}
+ */
 let homing;
 let destino;
 let confirmar_destino;
 let inicio;
 let orden_mover;
 
-// Array donde se guardan todas las variables que se leen del archivo "leer_variables.html"
+/**
+ * Array donde se guardan todas las variables que se leen del archivo "leer_variables.html".
+ * @type {string[]}
+ */
 let arrayVariablesPlc = new Array();
 
-//Intervalos para leer variables del plc
+/**
+ * Intervalo para leer variables del PLC.
+ * @type {number}
+ */
 setInterval(
+  /**
+   * Función para leer variables del PLC y actualizar el array.
+   * @param {Response} response - La respuesta de la solicitud HTTP.
+   * @param {string} response.text - Los datos del archivo "leer_variables.html".
+   */
   () =>
     fetch("sites/leer_variables.html")
       .then((response) => response.text())
@@ -38,6 +66,10 @@ setInterval(
   10
 );
 
+/**
+ * Intervalo para actualizar las variables del PLC.
+ * @type {number}
+ */
 setInterval(() => {
   homing = arrayVariablesPlc[0];
   destino = arrayVariablesPlc[1];
@@ -46,29 +78,70 @@ setInterval(() => {
   orden_mover = arrayVariablesPlc[4];
 }, 12);
 
-//Intervalo de animacion
+/**
+ * Intervalo de animación que llama a la función `movimientoCiclico`.
+ * @type {number}
+ */
 setInterval(() => movimientoCiclico(), 15);
 
-//Intervalos para verificar el estado de homing e inicio
+/**
+ * Intervalos para verificar el estado de homing e inicio.
+ */
+
+/**
+ * Intervalo que verifica el estado de homing e inicio cada 500 milisegundos.
+ */
 setInterval(() => {
+  /**
+   * Estado de homing. 1 si está activado, 0 si está desactivado.
+   * @type {number}
+   */
   if (homing == 1) {
     if (inicio == 1) {
+      /**
+       * Función que se ejecuta cuando el inicio está encendido.
+       * @function
+       */
       inicioEncendido();
     } else {
+      /**
+       * Función que se ejecuta cuando el inicio está apagado.
+       * @function
+       */
       inicioApagado();
     }
   }
 }, 500);
 
+/**
+ * Intervalo que verifica el estado de homing cada 1000 milisegundos.
+ */
 setInterval(() => {
+  /**
+   * Estado de homing. 1 si está activado, 0 si está desactivado.
+   * @type {number}
+   */
   if (homing == 1) {
+    /**
+     * Función que se ejecuta cuando el homing está encendido.
+     * @function
+     */
     homingEncendido();
   } else {
+    /**
+     * Función que se ejecuta cuando el homing está apagado.
+     * @function
+     */
     homingApagado();
   }
 }, 1000);
 
-//Funcion para mandar datos al plc
+/**
+ * Función para mandar datos al PLC.
+ * @param {string} variable - El nombre de la variable a enviar al PLC.
+ * @param {number} valor - El valor que se enviará a la variable.
+ * @function
+ */
 function mandarDatos(variable, valor) {
   fetch(window.location.href, {
     method: "POST",
@@ -79,7 +152,14 @@ function mandarDatos(variable, valor) {
   });
 }
 
-//Funciones para cambiar los colores y disponibilidad de los botones
+/**
+ * Funciones para cambiar los colores y disponibilidad de los botones.
+ */
+
+/**
+ * Función que se ejecuta cuando el inicio está encendido.
+ * @function
+ */
 function inicioEncendido() {
   btnConfirmarDestino.disabled = true;
   desplegableDestino.disabled = true;
@@ -87,6 +167,10 @@ function inicioEncendido() {
   btnParar.classList.remove("active");
 }
 
+/**
+ * Función que se ejecuta cuando el inicio está apagado.
+ * @function
+ */
 function inicioApagado() {
   btnConfirmarDestino.disabled = false;
   desplegableDestino.disabled = false;
@@ -94,6 +178,10 @@ function inicioApagado() {
   btnParar.classList.add("active");
 }
 
+/**
+ * Función que se ejecuta cuando el homing está encendido.
+ * @function
+ */
 function homingEncendido() {
   btnInicio.disabled = false;
   btnParar.disabled = false;
@@ -101,6 +189,10 @@ function homingEncendido() {
   leyendaPiloto.innerText = "Encendido";
 }
 
+/**
+ * Función que se ejecuta cuando el homing está apagado.
+ * @function
+ */
 function homingApagado() {
   btnInicio.disabled = true;
   btnParar.disabled = true;
@@ -110,11 +202,28 @@ function homingApagado() {
   leyendaPiloto.innerText = "Apagado";
 }
 
-//Funciones para la logica del movimiento ciclico
-
+/**
+ * Variable que almacena la posición actual de la animación cíclica.
+ * @type {number}
+ */
 let posicionActual = 0;
+
+/**
+ * Variable que indica si el tranvía ha entrado en la animación cíclica.
+ * @type {boolean}
+ */
 let entrado = false;
+
+/**
+ * Variable que indica si la animación cíclica ha sido ejecutada.
+ * @type {boolean}
+ */
 let ejecutado = false;
+
+/**
+ * Función que controla la lógica del movimiento cíclico del tranvía.
+ * @function
+ */
 function movimientoCiclico() {
   if (inicio == 1) {
     if (comprobarListoMoverCiclico()) {
@@ -135,38 +244,81 @@ function movimientoCiclico() {
   }
 }
 
+/**
+ * Función que comprueba si el tranvía está listo para moverse cíclicamente.
+ * @returns {boolean} `true` si el tranvía está listo para moverse, de lo contrario, `false`.
+ * @function
+ */
 function comprobarListoMoverCiclico() {
   return orden_mover == 1 ? true : false;
 }
 
+/**
+ * Función que inicia una animación en el tranvía en función de la posición actual.
+ * @param {number} posicionActual - La posición actual en la secuencia de animación.
+ * @function
+ */
 function empezarAnimacion(posicionActual) {
   btnInicio.classList.add("active");
   imgTranvia.classList.remove(...animacionesNombre);
   imgTranvia.classList.add(animacionesNombre[posicionActual]);
 }
 
-//Funciones para la logica del movimiento manual
+/**
+ * Array que almacena las posiciones coincidentes de destino para la animación manual.
+ * @type {number[]}
+ */
 const arrayCoincidentePosicion = [1, 2, 3, 4, 3, 2, 1, 0];
-function encontrarPosicionesDestino(destino) {
+
+/**
+ * Función que encuentra las posiciones coincidentes de destino en la animación manual.
+ * @param {number} destinoSeleccionado - El destino seleccionado.
+ * @returns {number[]} Un array con las posiciones coincidentes de destino.
+ * @function
+ */
+function encontrarPosicionesDestino(destinoSeleccionado) {
   const posiciones = [];
   for (let i = 0; i < arrayCoincidentePosicion.length; i++) {
-    if (arrayCoincidentePosicion[i] === destino) {
+    if (arrayCoincidentePosicion[i] === destinoSeleccionado) {
       posiciones.push(i);
     }
   }
   return posiciones;
 }
 
+/**
+ * Función que controla el movimiento manual del tranvía hacia un destino seleccionado.
+ * @param {number} destinoSeleccionado - El destino seleccionado.
+ * @function
+ */
 function movimientoManual(destinoSeleccionado) {
-  destinoPorcentajes = [19, 36, 53, 70, 86]; //Array con los porcentajes para el movimiento
+  /**
+   * Array con los porcentajes para el movimiento del tranvía hacia diferentes destinos.
+   * @type {number[]}
+   */
+  const destinoPorcentajes = [19, 36, 53, 70, 86];
+
+  /**
+   * Elimina todas las clases de animación del tranvía.
+   */
   imgTranvia.classList.remove(...animacionesNombre);
   imgTranvia.classList.remove("homePrimera");
-  //Variables con el left antes de entrar al switch con y sin el simbolo %
+
+  /**
+   * Almacena el valor de la propiedad `left` del tranvía antes de entrar al switch, con y sin el símbolo "%".
+   */
   let leftAnterior = imgTranvia.style.left;
   let leftAnteriorLimpio = leftAnterior.substring(0, leftAnterior.length - 1);
-  //Array con las posiciones de los dos posibles destinos de la animacion (ida y vuelta)
+
+  /**
+   * Array con las posiciones de los dos posibles destinos de la animación (ida y vuelta).
+   * @type {number[]}
+   */
   const posiciones = encontrarPosicionesDestino(Number(destinoSeleccionado));
-  //Switch que comprueba en el array anterior si el tranvia se mueve hacia delante o detras (eje y positivo o negativo)
+
+  /**
+   * Switch que determina el destino seleccionado y actualiza la posición del tranvía en consecuencia.
+   */
   switch (Number(destinoSeleccionado)) {
     case 0:
       imgTranvia.style.left = destinoPorcentajes[0] + "%";
@@ -201,25 +353,79 @@ function movimientoManual(destinoSeleccionado) {
     default:
       break;
   }
+
+  /**
+   * Indica que el tranvía ha entrado en la animación manual.
+   */
   entrado = true;
 }
 
-//Eventos relativos al click de botones o desplegables
+/**
+ * Evento click en el botón "Inicio". Envía un comando al PLC para iniciar el tranvía.
+ */
 btnInicio.addEventListener("click", () => {
+  /**
+   * Envía un comando al PLC para iniciar el tranvía.
+   * @function
+   */
   mandarDatos("INICIO", 1);
 });
-btnParar.addEventListener("click", () => mandarDatos("INICIO", 0));
+
+/**
+ * Evento click en el botón "Parar". Envía un comando al PLC para detener el tranvía.
+ */
+btnParar.addEventListener("click", () => {
+  /**
+   * Envía un comando al PLC para detener el tranvía.
+   * @function
+   */
+  mandarDatos("INICIO", 0);
+});
+
+/**
+ * Evento click en el botón "Confirmar Destino". Envía un comando al PLC para confirmar el destino seleccionado, inicia el movimiento manual, y guarda estadísticas.
+ */
 btnConfirmarDestino.addEventListener("click", () => {
+  /**
+   * Envía un comando al PLC para confirmar el destino seleccionado.
+   * @function
+   */
   mandarDatos("CONFIRMAR_DESTINO", 1);
+
+  /**
+   * Inicia el movimiento manual del tranvía hacia el destino seleccionado.
+   * @function
+   */
   movimientoManual(desplegableDestino.value);
+
+  /**
+   * Guarda estadísticas relacionadas con el destino seleccionado.
+   * @function
+   */
   guardarEstadisticas();
+
+  /**
+   * Envía un comando al PLC para desactivar la confirmación de destino después de un tiempo (500 ms).
+   */
   setTimeout(() => mandarDatos("CONFIRMAR_DESTINO", 0), 500);
 });
+
+/**
+ * Evento click en el desplegable de destinos. Envía el destino seleccionado al PLC.
+ */
 desplegableDestino.addEventListener("click", () => {
+  /**
+   * Envía el destino seleccionado al PLC.
+   * @function
+   */
   mandarDatos("DESTINO", desplegableDestino.value);
 });
 
-//Funciones para guardar las estadisticas en localStorage
+/**
+ * Función para guardar estadísticas en el almacenamiento local (localStorage).
+ * Si no existen estadísticas previas, se crea un objeto inicial. Si existen, se actualizan.
+ * @function
+ */
 let stats;
 function guardarEstadisticas() {
   if (localStorage.getItem("estadistica") == null) {
@@ -254,15 +460,38 @@ function guardarEstadisticas() {
   }
 }
 
-//Funciones para cambiar orden mover desde la web (Solo para realizar las pruebas)
+/**
+ * Función que cambia la orden de movimiento desde la web (solo para pruebas).
+ * @param {number} valor - El valor de la orden de movimiento (1 para activar, 0 para desactivar).
+ * @function
+ */
 document.getElementById("enOrdenMover").addEventListener("click", () => {
+  /**
+   * Cambia la orden de movimiento desde la web para activar el movimiento del tranvía.
+   * @function
+   */
   mandarDatos("ORDEN_MOVER", 1);
 });
 
+/**
+ * Función que cambia la orden de movimiento desde la web (solo para pruebas).
+ * @param {number} valor - El valor de la orden de movimiento (1 para activar, 0 para desactivar).
+ * @function
+ */
 document.getElementById("apOrdenMover").addEventListener("click", () => {
+  /**
+   * Cambia la orden de movimiento desde la web para desactivar el movimiento del tranvía.
+   * @function
+   */
   mandarDatos("ORDEN_MOVER", 0);
 });
 
+/**
+ * Intervalo que actualiza el valor de la orden de movimiento en la interfaz web.
+ */
 setInterval(() => {
+  /**
+   * Actualiza el valor de la orden de movimiento en la interfaz web.
+   */
   document.getElementById("tOrdenMover").innerHTML = orden_mover;
 }, 100);
